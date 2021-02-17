7dc1911bca7471cfb1d7e032bccd810172a2adce
Soyaeeb <monirul15-1742@diu.edu.bd>
2021-02-17 19:55:56 +0600

    Final Commit...

---
+++ app/src/main/java/com/monir/convertwebsiteintoapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.monir.convertwebsiteintoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private String url = "https://www.daraz.com.bd/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.web_view);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+        webView.setWebViewClient(new WebViewClient());
+
+        setTitle("Daraz");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+    }
+}
